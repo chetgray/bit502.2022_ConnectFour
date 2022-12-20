7b4faf8d63f72c29784218396140a4d8881d76f7
Chet Gray <chet.gray@waystar.com>
2022-12-20 16:29:58 -0500

    ✨ feat(Data): add new DTO classes
    These are direct correlaries of the data structure. Not sure if we should do that, or
    have DTOs have other DTOs as fields. We'll figure it out in action.
    
    Closes #299329

---
+++ ConnectFour.Data/DTOs/PlayerDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ConnectFour.Data.DTOs
+{
+    public class PlayerDTO
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public int RoomId { get; set; }
+        public int Num { get; set; }
+    }
+}
