87218d8eaa0ce732eac4992855c25e587b09de84
Clayton Hite <clayton.hite@waystar.com>
2022-12-20 09:30:50 -0500

    Created Turn Table with Unique Constraints and Foreign Keys

---
+++ ConnectFour.Database/Tables/Turn.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Turn]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RoomId] INT NULL, 
+    [Time] DATETIME2 NULL, 
+    [Num] INT NULL, 
+    [RowNum] INT NULL, 
+    [ColNum] INT NULL
+    CONSTRAINT UC_OneTurnPerRoom UNIQUE (RoomId, Num)
+    CONSTRAINT UC_RoomAndMoves UNIQUE (RoomId, RowNum, ColNum)
+    FOREIGN KEY (RoomId) REFERENCES Room(Id)
+)
