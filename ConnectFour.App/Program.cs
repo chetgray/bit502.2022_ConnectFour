b2d12b557841561c043a57f5c45515fa3292bc4d
Clayton Hite <clayton.hite@waystar.com>
2022-12-20 13:25:43 -0500

    Initial commit

--- ConnectFour.App/Program.cs
+++ ConnectFour.App/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace ConnectFour.App
+﻿using System;
+
+namespace ConnectFour.App
 {
     internal static class Program
     {
         static void Main()
         {
+            MainMenu();
+        }
+        private static void MainMenu()
+        {
+            bool isChoosing = true;
+            while (isChoosing)
+            {
+                Console.WriteLine("What would you like to do?\n[1] - New Single-Player Game\n[2] - New Multi-Player Game\n[3] - Join Multi-Player Game\n[4] - View Game Results");
+                string userResponse = Console.ReadLine();
+                isChoosing = false;
+                switch (userResponse)
+                {
+                    case "1":
+                        //New Single-Player Game
+                        break;
+                    case "2":
+                        //New Multi-Player Game
+                        break;
+                    case "3":
+                        //Join Multi-Player Game
+                        break;
+                    case "4":
+                        //View Game Results
+                        break;
+                    default:
+                        Console.WriteLine("Invalid response please choose 1, 2, 3, or 4");
+                        isChoosing = true;
+                        break;
+                }
+            }
         }
     }
 }
