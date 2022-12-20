a06e2f5f560f123698b297b240f507d9520d02bf
Chet Gray <chet.gray@waystar.com>
2022-12-20 16:16:36 -0500

    ✨ feat(Business): add new Model classes
    Closes #299340

---
+++ ConnectFour.Business/Models/PlayerModel.cs
@@ -0,0 +1,10 @@
+﻿using ConnectFour.Business.Models.Interfaces;
+
+namespace ConnectFour.Business.Models
+{
+    internal class PlayerModel : IPlayerModel
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
