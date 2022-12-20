0cc87d90bd7623fee294ca9ad129cdf599d786fb
Clayton Hite <clayton.hite@waystar.com>
2022-12-20 11:45:43 -0500

    Added test project

---
+++ ConnectFour.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConnectFour.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Waystar")]
+[assembly: AssemblyProduct("ConnectFour.Tests")]
+[assembly: AssemblyCopyright("Copyright © Waystar 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d3db19a-56ea-4936-858d-d3742a4ab991")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
