d92b2a739cc72c16ec6855dbffaf96bae5a50eb8
Chet Gray <chet.gray@waystar.com>
2022-12-21 13:09:38 -0500

    ✨ feat(Data): copy DAL code from chet.gray_Claims repo
    Closes #299217

---
+++ ConnectFour.Data/DALs/DAL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConnectFour.Data.DALs
+{
+    internal class DAL : IDAL
+    {
+        private readonly string _connectionString;
+
+        internal DAL(string connectionString)
+        {
+            _connectionString =
+                connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public DataTable ExecuteStoredProcedure(
+            string storedProcedureName,
+            Dictionary<string, object> parameters
+        )
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (
+                    SqlCommand command = new SqlCommand(storedProcedureName, connection)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    }
+                )
+                {
+                    foreach (KeyValuePair<string, object> parameter in parameters)
+                    {
+                        command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                    }
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        // Get the last DataTable in the result DataSet
+                        DataSet dataSet = new DataSet();
+                        adapter.Fill(dataSet);
+                        dataTable = dataSet.Tables[dataSet.Tables.Count - 1];
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+    }
+}
---
+++ ConnectFour.Data/DALs/IDAL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace ConnectFour.Data.DALs
+{
+    public interface IDAL
+    {
+        DataTable ExecuteStoredProcedure(
+            string storedProcedureName,
+            Dictionary<string, object> parameters
+        );
+    }
+}
