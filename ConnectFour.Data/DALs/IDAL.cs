f41d1dd92cd54b2e505d3e7519427c87d2dd131f
Chet Gray <chet.gray@waystar.com>
2022-12-21 15:09:38 -0500

    📝 docs(Data): document `ExecuteStoredProcedure`

--- ConnectFour.Data/DALs/IDAL.cs
+++ ConnectFour.Data/DALs/IDAL.cs
@@ -5,6 +5,16 @@ namespace ConnectFour.Data.DALs
 {
     public interface IDAL
     {
+        /// <summary>
+        /// Executes a stored procedure and returns the final result as a DataTable.
+        /// </summary>
+        /// <returns>The last DataTable in the result DataSet.</returns>
+        /// <param name="storedProcedureName">The name of the stored procedure to execute.</param>
+        /// <param name="parameters">The Dictionary of parameters to pass to the stored procedure.</param>
+        /// <remarks>
+        /// The stored procedure could <c>SELECT</c> multiple result tables, or nothing. The
+        /// desired result table should be the last <c>SELECT</c> in the stored procedure.
+        /// </remarks>
         DataTable ExecuteStoredProcedure(
             string storedProcedureName,
             Dictionary<string, object> parameters
