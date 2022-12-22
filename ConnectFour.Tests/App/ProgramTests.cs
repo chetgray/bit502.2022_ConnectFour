c9a99a40fd6a07f4d57e3125a22b22bc26be61c1
Clayton Hite <clayton.hite@waystar.com>
2022-12-22 11:55:40 -0500

    Merged PR 27555: US298661-Create Unit Test Project
    Unit test project for the Connect Four solution.
    
    - Merged with main after recent PR
    - Added Folders for App/Business/Data and Unit Tests
    - Added Test Project
    
    Related work items: #298661

---
+++ ConnectFour.Tests/App/ProgramTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConnectFour.Tests.App
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ConnectFour.Tests/Data/RoomRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConnectFour.Tests.Data
+{
+    [TestClass]
+    public class RoomRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
