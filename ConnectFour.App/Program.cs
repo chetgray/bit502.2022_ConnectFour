c71c7041b5f1c47a154b440a84c4df801e246ac0
Clayton Hite <clayton.hite@waystar.com>
2022-12-28 15:31:00 -0500

    Fixed error message not displaying and then refresh menu.

--- ConnectFour.App/Program.cs
+++ ConnectFour.App/Program.cs
@@ -42,26 +42,26 @@ namespace ConnectFour.App
                             isRunning = false;
                             break;
                         default:
+                            Console.Clear();
                             Console.WriteLine("Invalid response please choose 1, 2, 3, 4, or 5");
-                            Console.Write("--> ");
                             isChoosing = true;
                             break;
                     }
-                    Console.Clear();
-                    if (isQuitting == false)
+                }
+                Console.Clear();
+                if (isQuitting == false)
+                {
+                    WriteTitle();
+                    Console.WriteLine("What would you like to do?\n" +
+                    "[1] Return to the Main Menu\n" +
+                    "[2] Quit");
+                    Console.Write("--> ");
+                    string continueProgram = Console.ReadLine();
+                    if (continueProgram == "2")
                     {
-                        WriteTitle();
-                        Console.WriteLine("What would you like to do?\n" +
-                        "[1] Return to the Main Menu\n" +
-                        "[2] Quit");
-                        Console.Write("--> ");
-                        string continueProgram = Console.ReadLine();
-                        if (continueProgram == "2")
-                        {
-                            isRunning = false;
-                        }
-                        Console.Clear();
+                        isRunning = false;
                     }
+                    Console.Clear();
                 }
             }
         }
