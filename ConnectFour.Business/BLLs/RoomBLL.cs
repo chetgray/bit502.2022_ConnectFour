67772b8a7c7a00fc80b4ca8ba23415536c53417d
Chet Gray <chet.gray@waystar.com>
2023-01-03 15:07:44 -0500

    ✨ feat(Business): allow for passing an `IRoomRepository` to the `RoomBLL` constructor
    This will be used for unit testing `RoomBLL` with a `FakeRoomRepository`
    that implements `IRoomRepository`.

--- ConnectFour.Business/BLLs/RoomBLL.cs
+++ ConnectFour.Business/BLLs/RoomBLL.cs
@@ -2,11 +2,34 @@
 
 using ConnectFour.Business.Models.Interfaces;
 using ConnectFour.Data.DTOs;
+using ConnectFour.Data.Repositories;
+using ConnectFour.Data.Repositories.Interfaces;
 
 namespace ConnectFour.Business.BLLs
 {
     public class RoomBLL
     {
+        private IRoomRepository _repository;
+
+        /// <summary>
+        /// Creates a <see cref="RoomBLL"/> instance with a default <see cref="RoomRepository"/>
+        /// backend.
+        /// </summary>
+        public RoomBLL()
+        {
+            _repository = new RoomRepository();
+        }
+
+        /// <summary>
+        /// Creates a <see cref="RoomBLL"/> instance with the passed <paramref name="repository"/>
+        /// as the backend.
+        /// </summary>
+        /// <param name="repository">The <see cref="IRoomRepository"/> to use as the backend.</param>
+        public RoomBLL(IRoomRepository repository)
+        {
+            _repository = repository;
+        }
+
         private static RoomDTO ConvertToDto(IRoomModel model)
         {
             throw new NotImplementedException();
