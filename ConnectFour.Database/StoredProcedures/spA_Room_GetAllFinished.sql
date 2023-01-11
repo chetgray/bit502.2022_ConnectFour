a5dca526c5103987752c70f9f9d51b68a396f6af
Clayton Hite <clayton.hite@waystar.com>
2023-01-11 10:45:53 -0500

    Added SQL query to get custom result set for games that are finished.

---
+++ ConnectFour.Database/StoredProcedures/spA_Room_GetAllFinished.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[spA_Room_GetAllFinished]
+AS
+	SELECT	Room.Id				AS RoomId,
+			Room.CreationTime,
+			Room.ResultCode,
+			Player.Id			AS PlayerId,
+			Player.[Name],
+			Player.RoomId		AS PlayerRoomId,
+			Player.Num			AS PlayerNum,
+			Turn.Id				AS TurnId,
+			Turn.[Time],
+			Turn.RowNum,
+			Turn.ColNum,
+			Turn.RoomId			AS TurnRoomId,
+			Turn.Num			AS TurnNum
+	FROM	Room
+	INNER JOIN Player ON Player.RoomId = Room.Id
+	INNER JOIN Turn ON Turn.RoomId = Room.Id AND Turn.Num = Room.CurrentTurnNum
+	WHERE	Room.ResultCode > 0
+RETURN 0
