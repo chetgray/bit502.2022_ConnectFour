6a35ee4293577b657bd7b5f3f97907e3f6766e31
Clayton Hite <clayton.hite@waystar.com>
2023-01-11 10:50:39 -0500

    Updated using table array, getting the game durations, and displaying the results in a table format.

--- ConnectFour.App/Program.cs
+++ ConnectFour.App/Program.cs
@@ -13,44 +13,184 @@ namespace ConnectFour.App
         private static void Main()
         {
             DisplayResults(RoomBLL.GetAllFinished());
-
             Console.ReadKey();
         }
-        private static void DisplayResults(List<ResultModel> results)
+        private static void DisplayResults(List<IResultModel> results)
         {
-            int characterCounter = 0;
-            foreach (ResultModel result in results)
+            string[,] ResultTable = new string[results.Count + 1,7];
+            ResultTable[0, 0] = "Room ID";
+            ResultTable[0, 1] = "Started At";
+            ResultTable[0, 2] = "Duration";
+            ResultTable[0, 3] = "Player 1";
+            ResultTable[0, 4] = "Player 2";
+            ResultTable[0, 5] = "Winner";
+            ResultTable[0, 6] = "Number of Moves";
+            int amountOfResults = results.Count();
+
+            for (int i = 1; i <= amountOfResults; i++)
             {
-                for (int i = 0; i < result.Players.Count; i++)
+                int resultCode = (int)results[i - 1].ResultCode;
+                string winnerName = string.Empty;
+                if (resultCode > 0 && resultCode < 3)
                 {
-                   int playerNameLength = result.Players[i].Name.Length;
-                   if (playerNameLength > characterCounter)
-                   {
-                       characterCounter = playerNameLength;
-                   }
+                    winnerName = $"{results[i - 1].Players[resultCode - 1].Name}";
+                }
+                else if(resultCode == 0)
+                {
+                    winnerName = "DRAW";
+                }
+                else
+                {
+                    winnerName = "NULL";
+                }
+                ResultTable[i, 0] = results[i - 1].RoomId.ToString();
+                ResultTable[i, 1] = results[i - 1].CreationTime.ToString();
+                ResultTable[i, 2] = GetGameDuration(results[i-1].LastTurn.Time - results[i-1].CreationTime);
+                ResultTable[i, 3] = results[i - 1].Players[0].Name;
+                ResultTable[i, 4] = results[i - 1].Players[1].Name;
+                ResultTable[i, 5] = winnerName;
+                ResultTable[i, 6] = results[i - 1].LastTurn.Num.ToString();
+            }
+            
+            int rows = ResultTable.GetLength(0);
+            int columns = ResultTable.GetLength(1);
+            int[] widths = new int[columns];
+
+            for (int i = 0; i < columns; i++)
+            {
+                widths[i] = ResultTable[0, i].Length;
+
+                for (int j = 0; j < rows; j++)
+                {
+                    if (ResultTable[j, i].Length > widths[i])
+                    {
+                        widths[i] = ResultTable[j, i].Length;
+                    }
                 }
             }
 
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < characterCounter; i++)
+
+            for (int i = 0; i < rows; i++)
             {
-                sb.Append(" ");
+                if(i == 0)
+                {
+                    sb.Append(" ╔═");
+                    for (int j = 0; j < widths.Count(); j++)
+                    {
+                        sb.Append(new string('═', widths[j]));
+                        if (j < widths.Count() - 1)
+                        {
+                            sb.Append("═╦═");
+                        }
+                        else
+                        {
+                            sb.Append("═╗");
+                        }
+                    }
+                    Console.WriteLine(sb.ToString());
+                }
+                else
+                {
+                    sb.Clear();
+                    sb.Append(" ╠═");
+                    for (int j = 0; j < widths.Count(); j++)
+                    {
+                        sb.Append(new string('═', widths[j]));
+                        if (j < widths.Count() - 1)
+                        {
+                            sb.Append("═╬═");
+                        }
+                        else
+                        {
+                            sb.Append("═╣");
+                        }
+                    }
+                    Console.WriteLine(sb.ToString());
+                }  
+                
+                sb.Clear();
+                for (int j = 0; j < columns; j++)
+                {
+                    sb.Append(" ║ ");
+                    int columnWidth = widths[j];
+                    int stringLength = ResultTable[i, j].Length;
+                    int spacingBeforeAndAfter = columnWidth - stringLength;
+                    if(spacingBeforeAndAfter % 2 == 0)
+                    {
+                        sb.Append(' ', spacingBeforeAndAfter / 2);
+                        sb.Append(ResultTable[i, j]);
+                        sb.Append(' ', spacingBeforeAndAfter / 2);
+                    }
+                    else
+                    {
+                        sb.Append(" ");
+                        sb.Append(' ', spacingBeforeAndAfter / 2);
+                        sb.Append(ResultTable[i, j]);
+                        sb.Append(' ', spacingBeforeAndAfter / 2);                        
+                    }                    
+                }
+
+                sb.Append(" ║");
+                Console.WriteLine(sb.ToString());
             }
-            string characterExtender = sb.ToString();
-            Console.WriteLine("     Room   Started   Duration      Player 1 "+ characterExtender + "  Player 2  "+ characterExtender +"  Winner "+ characterExtender +"# of");
-            Console.WriteLine("       #      At                      Name     "+ characterExtender + "  Name    "+ characterExtender +"         "+ characterExtender +"Moves");
             sb.Clear();
-            for (int i = 0; i < characterCounter; i++)
+            sb.Append(" ╚═");
+            for (int j = 0; j < widths.Count(); j++)
             {
-                sb.Append("═");
+                sb.Append(new string('═', widths[j]));
+                if (j < widths.Count() - 1)
+                {
+                    sb.Append("═╩═");
+                }
+                else
+                {
+                    sb.Append("═╝");
+                }
             }
-            characterExtender = sb.ToString();
-            Console.WriteLine("    ╔═════╦═════════╦══════════╦═══════════"+ characterExtender + "╦══════════"+ characterExtender + "╦═════════"+ characterExtender + "╦═══════╗");
-
-            Console.WriteLine("    ╠═════╬═════════╬══════════╬═══════════"+ characterExtender + "╬══════════"+ characterExtender + "╬═════════"+ characterExtender + "╬═══════╣");
-
-            Console.WriteLine("    ╚═════╩═════════╩══════════╩═══════════"+ characterExtender + "╩══════════"+ characterExtender + "╩═════════"+ characterExtender + "╩═══════╝");
+            Console.WriteLine(sb.ToString());
         }
+
+        private static string GetGameDuration(TimeSpan duration)
+        { 
+            int days = (int)duration.TotalDays;
+            int hours = (int)duration.TotalHours;
+            int minutes = (int)duration.TotalMinutes;
+            if (days >= 1)
+            {
+                if (days > 1)
+                {
+                    return $"{days} Days";
+                }
+                else
+                {
+                    return $"{days} Day";
+                }
+            }
+            else if (hours >= 1)
+            {
+                if (hours > 1)
+                {
+                    return $"{hours} Hours";
+                }
+                else
+                {
+                    return $"{hours} Hour";
+                }
+            }
+            else
+            {
+                if (minutes > 1)
+                {
+                    return $"{minutes} Minutes";
+                }
+                else
+                {
+                    return $"{minutes} Minute";
+                }
+            }
+        }
+
         private static void DisplayBoard(IRoomModel room)
         {
             string p1 = $"░ {room.Players[0].Symbol} ░";
