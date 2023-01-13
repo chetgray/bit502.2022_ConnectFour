2f4683581d0e9ca3bf1dedc00fd0e1c58121cf1a
Clayton Hite <clayton.hite@waystar.com>
2023-01-13 10:57:47 -0500

    Added functionality for adding a player to an existing room

--- ConnectFour.Data/Repositories/PlayerRepository.cs
+++ ConnectFour.Data/Repositories/PlayerRepository.cs
@@ -1,15 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
-
+using ConnectFour.Data.DALs;
 using ConnectFour.Data.DTOs;
 
 namespace ConnectFour.Data.Repositories
 {
     public class PlayerRepository : BaseRepository
     {
-        private static PlayerDTO ConvertToDto(DataRow row)
+        public PlayerDTO AddPlayerToRoom(PlayerDTO dto)
         {
-            throw new NotImplementedException();
+            Dictionary<string, object> paramDictionary = new Dictionary<string, object>();
+            paramDictionary.Add("@Name", dto.Name);
+            paramDictionary.Add("@RoomId", dto.RoomId);
+            paramDictionary.Add("@PlayerNum", dto.Num);
+            DataTable dataTable = _dal.ExecuteStoredProcedure("dbo.spA_Player_AddPlayerToRoom", paramDictionary);
+            return ConvertToDto(dataTable.Rows[0]);
+        }
+        internal static PlayerDTO ConvertToDto(DataRow row)
+        {
+            PlayerDTO dto = new PlayerDTO
+            {
+                Id = (int)row["PlayerId"],
+                Name = (string)row["Name"],
+                RoomId = (int)row["PlayerRoomId"],
+                Num = (int)row["PlayerNum"]
+            };
+            return dto;
         }
     }
 }
