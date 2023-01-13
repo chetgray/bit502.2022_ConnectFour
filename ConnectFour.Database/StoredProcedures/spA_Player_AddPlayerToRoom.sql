9f4c0e9398f4d8244652ecd3eb708e2e6610295d
Clayton Hite <clayton.hite@waystar.com>
2023-01-13 11:00:02 -0500

    Added stored procedure for adding player to Room

---
+++ ConnectFour.Database/StoredProcedures/spA_Player_AddPlayerToRoom.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spA_Player_AddPlayerToRoom]
+	@RoomId int,
+	@PlayerNum int,
+	@Name nvarchar(4000)
+AS
+	DECLARE @PlayerId int
+
+	INSERT INTO Player ([Name], Num, RoomId)
+	VALUES (@Name, @PlayerNum, @RoomId)
+	SET @PlayerId = SCOPE_IDENTITY()
+
+	SELECT	Player.Id		AS PlayerId,
+			Player.[Name],
+			Player.Num		AS PlayerNum,
+			Player.RoomID	AS PlayerRoomId
+
+	FROM Player WHERE Player.Id = @PlayerId
