b84d1c81e93c22dcbfde86108a5badac8ed62ce8
Clayton Hite <clayton.hite@waystar.com>
2023-01-13 10:55:16 -0500

    added nullable to RoomId

--- ConnectFour.Data/DTOs/PlayerDTO.cs
+++ ConnectFour.Data/DTOs/PlayerDTO.cs
@@ -5,7 +5,7 @@
         public int? Id { get; set; }
         public string Name { get; set; } = string.Empty;
 
-        public int RoomId { get; set; }
+        public int? RoomId { get; set; }
         public int Num { get; set; }
     }
 }
