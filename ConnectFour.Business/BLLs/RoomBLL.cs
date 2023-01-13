5740ff4e13e1debfd240b025f30fba5513c4cf46
Clayton Hite <clayton.hite@waystar.com>
2023-01-13 10:47:48 -0500

    Added method to get the Room and Players associated. Added converting to Model.

--- ConnectFour.Business/BLLs/RoomBLL.cs
+++ ConnectFour.Business/BLLs/RoomBLL.cs
@@ -1,20 +1,46 @@
 ﻿using System;
-
+using ConnectFour.Business.Models;
 using ConnectFour.Business.Models.Interfaces;
 using ConnectFour.Data.DTOs;
+using ConnectFour.Data.Repositories;
 
 namespace ConnectFour.Business.BLLs
 {
     public class RoomBLL
     {
-        private static PlayerDTO ConvertToDto(IPlayerModel playerModel)
+        public IRoomModel GetRoomOccupancy(int roomId)
         {
-            throw new NotImplementedException();
+            RoomRepository roomRepo = new RoomRepository();
+            IRoomModel room = ConvertRoomDTOToModel(roomRepo.GetRoomOccupancy(roomId));
+            return room;
+        }
+
+        private static PlayerModel ConvertPlayerDTOToModel(PlayerDTO dto)
+        {
+            PlayerModel pM = new PlayerModel();
+            pM.Id = dto.Id;
+            pM.Name = dto.Name;
+            pM.Num = dto.Num;
+            return pM;
         }
 
         private static IPlayerModel ConvertToModel(PlayerDTO dto)
         {
             throw new NotImplementedException();
+        }
+        private static RoomModel ConvertRoomDTOToModel(RoomDTO roomDTO)
+        {
+            RoomModel rM = new RoomModel();
+            rM.Id = roomDTO.Id;
+            rM.CreationTime = roomDTO.CreationTime;
+            rM.CurrentTurnNum = roomDTO.CurrentTurnNumber;
+            rM.ResultCode = roomDTO.ResultCode;
+            foreach (PlayerDTO pDTO in roomDTO.Players)
+            {
+                rM.Players.Add(ConvertPlayerDTOToModel(pDTO));
+            }
+            rM.Vacancy = (rM.Players.Count < 2) ? true : false;            
+            return rM;
         }
     }
 }
