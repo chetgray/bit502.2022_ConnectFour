7c8451c7b35dfafe8ced1b4cc3f419c7a6ab12d7
Clayton Hite <clayton.hite@waystar.com>
2023-01-17 16:00:42 -0500

    brought back only the data neccesary and worked with that in the repo

--- ConnectFour.Data/Repositories/RoomRepository.cs
+++ ConnectFour.Data/Repositories/RoomRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+
 using ConnectFour.Data.DTOs;
 
 namespace ConnectFour.Data.Repositories
@@ -17,33 +18,33 @@ namespace ConnectFour.Data.Repositories
             for (int i = 0; i < dataTable.Rows.Count; i++)
             {
                 DataRow row = dataTable.Rows[i];
-                int roomId = (int)row["RoomId"];
-                if (resultDTODictionary.ContainsKey(roomId))
-                {
-                    resultDTODictionary[roomId].Players.Add(PlayerRepository.ConvertToDto(row));
-                    resultDTODictionary[roomId].Players.Sort((x, y) => x.Num.CompareTo(y.Num));
-                }
-                else
-                {
-                    resultDTODictionary.Add(roomId, new ResultDTO
-                    {
-                        RoomId = roomId,
-                        CreationTime = (DateTime)row["RoomCreationTime"],
-                        ResultCode = (int)row["RoomResultCode"],
-                        LastTurn = TurnRepository.ConvertToDto(row),
-                        Players = new List<PlayerDTO>
-                        {
-                            PlayerRepository.ConvertToDto(row)
-                        }
-                    });
-                }
+                resultDTODictionary = ConvertToDto(row, resultDTODictionary);
             }
 
             List<ResultDTO> resultDTOS = resultDTODictionary.Values.ToList();
-
             return resultDTOS;
         }
-        
+        private static Dictionary<int, ResultDTO> ConvertToDto(DataRow row, Dictionary<int, ResultDTO> resultDTODictionary)
+        {
+            int roomId = (int)row["RoomId"];
+            if (resultDTODictionary.ContainsKey(roomId))
+            {
+                resultDTODictionary[roomId].Players.Add((int)row["PlayerNum"], row["PlayerName"].ToString());
+            }
+            else
+            {
+                resultDTODictionary.Add(roomId, new ResultDTO
+                {
+                    RoomId = roomId,
+                    CreationTime = (DateTime)row["RoomCreationTime"],
+                    ResultCode = (int)row["RoomResultCode"],
+                    LastTurnTime = (DateTime)row["TurnTime"],
+                    LastTurnNum = (int)row["TurnNum"]
+                });
+                resultDTODictionary[roomId].Players.Add((int)row["PlayerNum"], row["PlayerName"].ToString());
+            }
+            return resultDTODictionary;
+        }
         private static RoomDTO ConvertToDto(DataRow row)
         {
             throw new NotImplementedException();
--- ConnectFour.Database/StoredProcedures/spA_Room_GetAllFinished.sql
+++ ConnectFour.Database/StoredProcedures/spA_Room_GetAllFinished.sql
@@ -3,15 +3,9 @@ AS
 	SELECT	Room.Id				AS RoomId,
 			Room.CreationTime	AS RoomCreationTime,
 			Room.ResultCode		AS RoomResultCode,
-			Player.Id			AS PlayerId,
 			Player.[Name]		AS PlayerName,
-			Player.RoomId		AS PlayerRoomId,
 			Player.Num			AS PlayerNum,
-			Turn.Id				AS TurnId,
 			Turn.[Time]			AS TurnTime,
-			Turn.RowNum			AS TurnRowNum,
-			Turn.ColNum			AS TurnColNum,
-			Turn.RoomId			AS TurnRoomId,
 			Turn.Num			AS TurnNum
 	FROM	Room
 	INNER JOIN Player ON Player.RoomId = Room.Id
