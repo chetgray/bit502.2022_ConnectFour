cd93630358e967f8202fca5fdb6d69990d99f22f
Clayton Hite <clayton.hite@waystar.com>
2023-01-26 13:15:00 -0500

    Refactored while loop to minimum, handle with switch statement on what to do next.

--- ConnectFour.App/Program.cs
+++ ConnectFour.App/Program.cs
@@ -10,62 +10,41 @@ namespace ConnectFour.App
     {
         private static void Main()
         {
-            bool isRunning = true;
-            while (isRunning)
+            bool isChoosing = true;
+            while (isChoosing)
             {
-                bool isChoosing = true;
-                bool isQuitting = false;
-                while (isChoosing)
-                {
-                    WriteTitle();
-                    Console.WriteLine("What would you like to do?\n" +
-                    "[1] - New Single-Player Game\n" +
-                    "[2] - New Multi-Player Game\n" +
-                    "[3] - Join Multi-Player Game\n" +
-                    "[4] - View Game Results\n" +
-                    "[5] - Quit\n");
-                    Console.Write("--> ");
-                    string userResponse = Console.ReadLine();
-                    isChoosing = false;
-                    switch (userResponse)
-                    {
-                        case "1":
-                            //New Single-Player Game
-                            break;
-                        case "2":
-                            //New Multi-Player Game
-                            break;
-                        case "3":
-                            //Join Multi-Player Game
-                            break;
-                        case "4":
-                            //View Game Results
-                            break;
-                        case "5":
-                            isQuitting = true;
-                            isRunning = false;
-                            break;
-                        default:
-                            Console.Clear();
-                            Console.WriteLine("Invalid response please choose 1, 2, 3, 4, or 5");
-                            isChoosing = true;
-                            break;
-                    }
-                }
                 Console.Clear();
-                if (isQuitting == false)
+                WriteTitle();
+                Console.WriteLine("What would you like to do?\n" +
+                "[1] - New Single-Player Game\n" +
+                "[2] - New Multi-Player Game\n" +
+                "[3] - Join Multi-Player Game\n" +
+                "[4] - View Game Results\n" +
+                "[5] - Quit\n");
+                Console.Write("--> ");
+                string userResponse = Console.ReadLine();
+                switch (userResponse)
                 {
-                    WriteTitle();
-                    Console.WriteLine("What would you like to do?\n" +
-                    "[1] Return to the Main Menu\n" +
-                    "[2] Quit");
-                    Console.Write("--> ");
-                    string continueProgram = Console.ReadLine();
-                    if (continueProgram == "2")
-                    {
-                        isRunning = false;
-                    }
-                    Console.Clear();
+                    case "1":
+                        //New Single-Player Game
+                        break;
+                    case "2":
+                        //New Multi-Player Game
+                        break;
+                    case "3":
+                        //Join Multi-Player Game
+                        break;
+                    case "4":
+                        //View Game Results
+                        break;
+                    case "5":
+                        isChoosing = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid response please choose 1, 2, 3, 4, or 5");
+                        isChoosing = true;
+                        break;
                 }
             }
 
