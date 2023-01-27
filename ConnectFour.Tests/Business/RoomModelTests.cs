863f7ffe117dc84452eaa87dbd3820c996ea6e42
Chet Gray <chet.gray@waystar.com>
2023-01-27 17:14:04 -0500

    🚨 test(Business): combine horizontal test cases into data-driven test

--- ConnectFour.Tests/Business/RoomModelTests.cs
+++ ConnectFour.Tests/Business/RoomModelTests.cs
@@ -8,93 +8,44 @@ namespace ConnectFour.Tests.Business
     public class RoomModelTests
     {
         [TestMethod]
-        public void TestFourthPieceToRightWins()
+        [DataRow(1, 1)]
+        [DataRow(2, 1)]
+        [DataRow(2, 2)]
+        [DataRow(3, 1)]
+        [DataRow(3, 2)]
+        [DataRow(3, 3)]
+        [DataRow(4, 1)]
+        [DataRow(4, 2)]
+        [DataRow(4, 3)]
+        [DataRow(4, 4)]
+        [DataRow(5, 2)]
+        [DataRow(5, 3)]
+        [DataRow(5, 4)]
+        [DataRow(6, 3)]
+        [DataRow(6, 4)]
+        [DataRow(7, 4)]
+        public void TestHorizontalWin(int colNum, int positionWithinFour)
         {
             // Arrange
-            RoomModel room = new RoomModel();
-            room.Players.Add(new PlayerModel());
-            room.Players.Add(new PlayerModel());
-            room.Board[5, 0] = 1;
-            room.Board[5, 1] = 1;
-            room.Board[5, 2] = 1;
             TurnModel turn = new TurnModel
             {
                 Num = 1, // Player 1
                 RowNum = 6,
-                ColNum = 4
+                ColNum = colNum
             };
-
-            // Act
-            bool result = room.CheckForWin(turn);
-
-            // Assert
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void TestFourthPieceToLeftWins()
-        {
-            // Arrange
             RoomModel room = new RoomModel();
             room.Players.Add(new PlayerModel());
             room.Players.Add(new PlayerModel());
-            room.Board[5, 1] = 1;
-            room.Board[5, 2] = 1;
-            room.Board[5, 3] = 1;
-            TurnModel turn = new TurnModel
+            // Set up board to the left of turn
+            for (int c = colNum - (positionWithinFour - 1) - 1; c < colNum - 1; c++)
             {
-                Num = 1, // Player 1
-                RowNum = 6,
-                ColNum = 1
-            };
-
-            // Act
-            bool result = room.CheckForWin(turn);
-
-            // Assert
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void TestMiddleLeftPieceWins()
-        {
-            // Arrange
-            RoomModel room = new RoomModel();
-            room.Players.Add(new PlayerModel());
-            room.Players.Add(new PlayerModel());
-            room.Board[5, 0] = 1;
-            room.Board[5, 2] = 1;
-            room.Board[5, 3] = 1;
-            TurnModel turn = new TurnModel
+                room.Board[5, c] = 1;
+            }
+            // Set up board to the right of turn
+            for (int c = colNum + 1 - 1; c <= (colNum - 1) + (4 - positionWithinFour); c++)
             {
-                Num = 1, // Player 1
-                RowNum = 6,
-                ColNum = 2
-            };
-
-            // Act
-            bool result = room.CheckForWin(turn);
-
-            // Assert
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void TestMiddleRightPieceWins()
-        {
-            // Arrange
-            RoomModel room = new RoomModel();
-            room.Players.Add(new PlayerModel());
-            room.Players.Add(new PlayerModel());
-            room.Board[5, 0] = 1;
-            room.Board[5, 1] = 1;
-            room.Board[5, 3] = 1;
-            TurnModel turn = new TurnModel
-            {
-                Num = 1, // Player 1
-                RowNum = 6,
-                ColNum = 3
-            };
+                room.Board[5, c] = 1;
+            }
 
             // Act
             bool result = room.CheckForWin(turn);
