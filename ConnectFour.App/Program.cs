794da9c3ebb624bd298e774194c036df0e4f3239
Clayton Hite <clayton.hite@waystar.com>
2023-02-09 10:25:48 -0500

    removed pausing the thread after input spam since clearing keys will add a small wait time. Added barrier for backspace input so it will stay at the cursor point desired if nothing is entered abd backspace is being pressed.

--- ConnectFour.App/Program.cs
+++ ConnectFour.App/Program.cs
@@ -162,9 +162,9 @@ namespace ConnectFour.App
         /// <summary>
         /// Allows user to type a string with the ability to edit it, press enter to return the string, or press escape to return a null string.
         /// </summary>
-        /// <param name="inputLineWidth">The character location from the left of the window for the cursor on the line</param>
+        /// <param name="inputLineWidth">The character location from the left of the window for the cursor on the line. Starts at 1 value.</param>
         /// <param name="inputLineFromTopLine">The line from the first line on the top of the window that the user will write on. Starts at 0 value.</param>
-        /// <returns></returns>
+        /// <returns>String if enter is pressed and null if escape is pressed</returns>
         private static string GetUserInput(int inputLineWidth, int inputLineFromTopLine)
         {
             Console.CursorTop = inputLineFromTopLine;
@@ -175,13 +175,9 @@ namespace ConnectFour.App
             {
                 Console.CursorLeft = inputLineWidth;
                 //This blocks the user from spamming and clears input stream (KeyAvailable) up to this point
-                Thread.Sleep(2000);
                 while (Console.KeyAvailable)
-                {                    
-                    Console.ReadKey(false);
-                    Console.CursorLeft = inputLineWidth;
-                    Console.Write(" ");
-                    Console.CursorLeft = inputLineWidth;
+                {
+                    Console.ReadKey(true);
                 }
             }
             while (input.Key != ConsoleKey.Enter && input.Key != ConsoleKey.Escape)
@@ -198,12 +194,16 @@ namespace ConnectFour.App
                         sb.Clear();
                         sb.Append(removeChar);
                     }
+                    else
+                    {
+                        Console.CursorLeft = inputLineWidth;
+                    }
                 }
                 else if (input.Key != ConsoleKey.Enter && input.Key != ConsoleKey.Escape)
                 {
                     sb.Append(input.KeyChar);
                 }
-            };
+            }
             if (input.Key == ConsoleKey.Escape)
             {
                 return null;
