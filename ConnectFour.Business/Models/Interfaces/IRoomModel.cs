f44c09127a7455b5e2da21bf87a2bd299c82c35f
Chet Gray <Chet.Gray@waystar.com>
2023-03-28 16:35:26 -0400

    Merged PR 31539: ✨ feat: Check board for win (US 298786)
    - ♻ refactor(App,Business): represent board pieces with `int`s
    - ✨ feat(Business): implement CheckForWin horizontally and vertically
    - 🚨 test(Business,Test): orthogonal tests for `RoomModel.CheckForWin`
    - 🚨 test(Business): combine horizontal test cases into data-driven test
    - ✅ test(Business): four-in-a-row interrupted by opponent won't win
    - ✅ test(Business): test horizontal win on different rows
    - ✅ chore(Tests): rename and organize tests
    - ⚡️ perf(Business): don't check for win above piece
    - ✅ test(Business): play in occupied cell won't win
    - ✅ refactor(Tests): simplify parameterized test
    - 🙈 chore: ignore live unit testing config
    - ✅ feat(Business): fourth piece diagonal will win
    - ✅ build(Tests): remove reference to sqlproj so live testing works
    - ♻ refactor(Business): combine four-in-a-row check loops
    
    Closes #298786
    
    Related work items: #298786

--- ConnectFour.Business/Models/Interfaces/IRoomModel.cs
+++ ConnectFour.Business/Models/Interfaces/IRoomModel.cs
@@ -13,8 +13,23 @@ namespace ConnectFour.Business.Models.Interfaces
         IPlayerModel[] Players { get; }
         List<ITurnModel> Turns { get; }
 
-        string[,] Board { get; set; }
+        /// <summary>
+        /// Gets or sets the representation of the board grid's current state.
+        /// </summary>
+        /// <value>
+        /// A two-dimensional array representing the pieces in each cell.
+        /// </value>
+        int[,] Board { get; set; }
         bool Vacancy { get; set; }
         string Message { get; set; }
+
+        /// <summary>
+        /// Checks if the given <paramref name="turn"/> will result in a win.
+        /// </summary>
+        /// <returns>
+        /// <see langword="true"/> if the <paramref name="turn"/> will result in a win, otherwise <see langword="false"/>.
+        /// </returns>
+        /// <param name="turn">The <see cref="ITurnModel"/> to check.</param>
+        bool CheckForWin(ITurnModel turn);
     }
 }
--- ConnectFour.Business/Models/RoomModel.cs
+++ ConnectFour.Business/Models/RoomModel.cs
@@ -15,9 +15,60 @@ namespace ConnectFour.Business.Models
         public IPlayerModel[] Players { get; set; } = new IPlayerModel[2];
         public List<ITurnModel> Turns { get; set; } = new List<ITurnModel>();
 
-        public string[,] Board { get; set; } = new string[6, 7];
+        public int[,] Board { get; set; } = new int[6, 7];
         public bool Vacancy { get; set; }
 
         public string Message { get; set; }
+
+        public bool CheckForWin(ITurnModel turn)
+        {
+            // Bail if the turn's cell is already occupied.
+            if (Board[turn.RowNum - 1, turn.ColNum - 1] != 0)
+            {
+                return false;
+            }
+
+            int playerNum = ((turn.Num - 1) % Players.Length) + 1;
+
+            // Check for four-in-a-row in each direction.
+            foreach (
+                (int rowDir, int colDir) in new (int, int)[]
+                {
+                    (-1, -1), // ↖
+                    (-1, 1), // ↗
+                    (0, 1), // →
+                    (-1, 0), // ↑
+                }
+            )
+            {
+                int count = 0;
+                for (int i = -3; i <= 3; i++)
+                {
+                    int r = turn.RowNum + (rowDir * i) - 1;
+                    int c = turn.ColNum + (colDir * i) - 1;
+                    // Skip to the next cell if we're outside the board.
+                    if (r < 0 || r >= Board.GetLength(0) || c < 0 || c >= Board.GetLength(1))
+                    {
+                        continue;
+                    }
+                    // Reset count if this isn't our piece (aside from the new piece's cell).
+                    if (
+                        Board[r, c] != playerNum
+                        && (r != turn.RowNum - 1 || c != turn.ColNum - 1)
+                    )
+                    {
+                        count = 0;
+                        continue;
+                    }
+                    count++;
+                    if (count == 4)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
     }
 }
