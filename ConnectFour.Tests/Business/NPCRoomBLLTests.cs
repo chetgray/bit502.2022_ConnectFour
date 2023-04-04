a1ba16f3af4db398b4e9d508f421aa9b355ab31e
Jess Eversole <jess.eversole@waystar.com>
2023-04-04 14:43:59 -0400

    Merged PR 31770: Implement random NPC turn
    - [x] Should only choose a valid column (not full, inside bounds)
    - [x] Choose a random valid column
    - [x] Return ~~column num or~~ `TurnModel`
    
    - Utilizes existing `GetNextRowInCol()` method to check validity of random row selection
    
    Related work items: #298794

---
+++ ConnectFour.Tests/Business/NPCRoomBLLTests.cs
@@ -0,0 +1,77 @@
+﻿using ConnectFour.Business.BLLs;
+using ConnectFour.Business.BLLs.Interfaces;
+using ConnectFour.Business.Models;
+using ConnectFour.Business.Models.Interfaces;
+using ConnectFour.Data.Repositories.Interfaces;
+using ConnectFour.Tests.TestDoubles;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConnectFour.Tests.Business
+{
+    [TestClass]
+    public class NPCRoomBLLTests
+    {
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(5)]
+        [DataRow(6)]
+        [DataRow(7)]
+        [TestMethod]
+        public void GetValidPlay_LastOpenColumn_LastOpenSpaceReturned(int colNum)
+        {
+            // Arrange
+            IRoomModel room = new RoomModel()
+            {
+                Board = new int[,]
+                { //                        index / RowNum
+                    { 0, 0, 0, 0, 0, 0, 0 }, // 0 / 1
+                    { 2, 1, 2, 1, 2, 1, 2 }, // 1 / 2
+                    { 2, 1, 2, 1, 2, 1, 2 }, // 2 / 3
+                    { 1, 2, 1, 2, 1, 2, 1 }, // 3 / 4
+                    { 2, 1, 2, 1, 2, 1, 2 }, // 4 / 5
+                    { 2, 1, 2, 1, 2, 1, 2 }, // 5 / 6
+                    //0, 1, 2, 3, 4, 5, 6 - index
+                    //1, 2, 3, 4, 5, 6, 7 - ColNum
+                }
+            };
+
+            for (int i = 1; i <= 7; i++)
+            {
+                if (i != colNum)
+                {
+                    room.Board[0, i - 1] = (i % 2) + 1;
+                }
+            }
+
+            for (int row = 0; row < room.Board.GetLength(0); row++)
+            {
+                for (int column = 0; column < room.Board.GetLength(1); column++)
+                {
+                    if (room.Board[row, column] != 0)
+                    {
+                        room.Turns.Add(new TurnModel
+                        {
+                            Id = null,
+                            ColNum = column + 1,
+                            RowNum = row + 1,
+                            Num = room.Board[row, column]
+                        });
+                    }
+                }
+            }
+
+            // Act
+            IRoomRepository repository = new RoomRepositoryStub();
+            IPlayerBLL playerBLL = new PlayerBLLStub();
+            ITurnBLL turnBLL = new TurnBLLStub();
+            NPCRoomBLL bll = new NPCRoomBLL(repository, playerBLL, turnBLL);
+            TurnModel turn = bll.RandyTakesATurn(room);
+
+            // Assert
+            Assert.AreEqual(turn.ColNum, colNum);
+        }
+    }
+}
