b3885b3ce26bde22438842096cba9800bb3acfb3
Chet Gray <chet.gray@waystar.com>
2023-04-05 09:04:09 -0400

    ♻️ refactor(Tests): extract method `GenerateTurnsFromBoard`

--- ConnectFour.Tests/Business/NPCRoomBLLTests.cs
+++ ConnectFour.Tests/Business/NPCRoomBLLTests.cs
@@ -26,45 +26,27 @@ namespace ConnectFour.Tests.Business
         public void GetValidPlay_LastOpenColumn_LastOpenSpaceReturned(int colNum)
         {
             // Arrange
-            IRoomModel room = new RoomModel()
+            int[,] board = new int[,]
             {
-                Board = new int[,]
-                { //                        index / RowNum
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 0 / 1
-                    { 2, 1, 2, 1, 2, 1, 2 }, // 1 / 2
-                    { 2, 1, 2, 1, 2, 1, 2 }, // 2 / 3
-                    { 1, 2, 1, 2, 1, 2, 1 }, // 3 / 4
-                    { 2, 1, 2, 1, 2, 1, 2 }, // 4 / 5
-                    { 2, 1, 2, 1, 2, 1, 2 }, // 5 / 6
-                    //0, 1, 2, 3, 4, 5, 6 - index
-                    //1, 2, 3, 4, 5, 6, 7 - ColNum
-                }
+                //                      index / RowNum
+                { 0, 0, 0, 0, 0, 0, 0 }, // 0 / 1
+                { 2, 1, 2, 1, 2, 1, 2 }, // 1 / 2
+                { 2, 1, 2, 1, 2, 1, 2 }, // 2 / 3
+                { 1, 2, 1, 2, 1, 2, 1 }, // 3 / 4
+                { 2, 1, 2, 1, 2, 1, 2 }, // 4 / 5
+                { 2, 1, 2, 1, 2, 1, 2 }, // 5 / 6
+                //0, 1, 2, 3, 4, 5, 6 - index
+                //1, 2, 3, 4, 5, 6, 7 - ColNum
             };
-
             for (int i = 1; i <= 7; i++)
             {
                 if (i != colNum)
                 {
-                    room.Board[0, i - 1] = (i % 2) + 1;
+                    board[0, i - 1] = (i % 2) + 1;
                 }
             }
-
-            for (int row = 0; row < room.Board.GetLength(0); row++)
-            {
-                for (int column = 0; column < room.Board.GetLength(1); column++)
-                {
-                    if (room.Board[row, column] != 0)
-                    {
-                        room.Turns.Add(new TurnModel
-                        {
-                            Id = null,
-                            ColNum = column + 1,
-                            RowNum = row + 1,
-                            Num = room.Board[row, column]
-                        });
-                    }
-                }
-            }
+            List<ITurnModel> turns = GenerateTurnsFromBoard(board);
+            IRoomModel room = new RoomModel() { Turns = turns, Board = board };
 
             // Act
             IRoomRepository repository = new RoomRepositoryStub();
@@ -227,39 +209,25 @@ namespace ConnectFour.Tests.Business
         public void OpheliaTakesATurn_BlockingPlayAvailable_ReturnsBlockingTurn()
         {
             // Arrange
+            int[,] board = new int[,]
+            {
+                //                      index / RowNum
+                { 0, 0, 0, 0, 0, 0, 0 }, // 0 / 1
+                { 0, 0, 0, 0, 0, 0, 0 }, // 1 / 2
+                { 0, 0, 0, 0, 0, 0, 0 }, // 2 / 3
+                { 0, 0, 0, 0, 0, 0, 0 }, // 3 / 4
+                { 2, 2, 0, 0, 0, 0, 0 }, // 4 / 5
+                { 1, 1, 1, 0, 0, 0, 0 }, // 5 / 6
+                //0, 1, 2, 3, 4, 5, 6 - index
+                //1, 2, 3, 4, 5, 6, 7 - ColNum
+            };
+            List<ITurnModel> turns = GenerateTurnsFromBoard(board);
             IRoomModel room = new RoomModel
             {
-                Board = new int[,]
-                {
-                    //                      index / RowNum
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 0 / 1
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 1 / 2
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 2 / 3
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 3 / 4
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 4 / 5
-                    { 1, 1, 1, 0, 0, 0, 0 }, // 5 / 6
-                    //0, 1, 2, 3, 4, 5, 6 - index
-                    //1, 2, 3, 4, 5, 6, 7 - ColNum
-                },
                 CurrentTurnNum = 6,
+                Turns = turns,
+                Board = board,
             };
-            for (int r = 0; r < room.Board.GetLength(0); r++)
-            {
-                for (int c = 0; c < room.Board.GetLength(1); c++)
-                {
-                    if (room.Board[r, c] != 0)
-                    {
-                        room.Turns.Add(
-                            new TurnModel
-                            {
-                                ColNum = c + 1,
-                                RowNum = r + 1,
-                                Num = room.Board[r, c]
-                            }
-                        );
-                    }
-                }
-            }
             IRoomRepository repository = new RoomRepositoryStub();
             IPlayerBLL playerBLL = new PlayerBLLStub();
             ITurnBLL turnBLL = new TurnBLLStub();
@@ -278,39 +246,25 @@ namespace ConnectFour.Tests.Business
         public void OpheliaTakesATurn_WinningPlayAvailable_ReturnsWinningTurn()
         {
             // Arrange
+            int[,] board = new int[,]
+            {
+                //                      index / RowNum
+                { 0, 0, 0, 0, 0, 0, 0 }, // 0 / 1
+                { 0, 0, 0, 0, 0, 0, 0 }, // 1 / 2
+                { 0, 0, 0, 0, 0, 0, 0 }, // 2 / 3
+                { 1, 0, 0, 0, 0, 0, 0 }, // 3 / 4
+                { 1, 0, 0, 0, 0, 0, 0 }, // 4 / 5
+                { 1, 2, 0, 2, 0, 0, 2 }, // 5 / 6
+                //0, 1, 2, 3, 4, 5, 6 - index
+                //1, 2, 3, 4, 5, 6, 7 - ColNum
+            };
+            List<ITurnModel> turns = GenerateTurnsFromBoard(board);
             IRoomModel room = new RoomModel
             {
-                Board = new int[,]
-                {
-                    //                      index / RowNum
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 0 / 1
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 1 / 2
-                    { 0, 0, 0, 0, 0, 0, 0 }, // 2 / 3
-                    { 1, 0, 0, 0, 0, 0, 0 }, // 3 / 4
-                    { 1, 0, 0, 0, 0, 0, 0 }, // 4 / 5
-                    { 1, 0, 0, 0, 0, 0, 0 }, // 5 / 6
-                    //0, 1, 2, 3, 4, 5, 6 - index
-                    //1, 2, 3, 4, 5, 6, 7 - ColNum
-                },
                 CurrentTurnNum = 7,
+                Turns = turns,
+                Board = board,
             };
-            for (int r = 0; r < room.Board.GetLength(0); r++)
-            {
-                for (int c = 0; c < room.Board.GetLength(1); c++)
-                {
-                    if (room.Board[r, c] != 0)
-                    {
-                        room.Turns.Add(
-                            new TurnModel
-                            {
-                                ColNum = c + 1,
-                                RowNum = r + 1,
-                                Num = room.Board[r, c]
-                            }
-                        );
-                    }
-                }
-            }
             IRoomRepository repository = new RoomRepositoryStub();
             IPlayerBLL playerBLL = new PlayerBLLStub();
             ITurnBLL turnBLL = new TurnBLLStub();
@@ -323,6 +277,29 @@ namespace ConnectFour.Tests.Business
             // Assert
             Assert.AreEqual(expected.RowNum, actual.RowNum);
             Assert.AreEqual(expected.ColNum, actual.ColNum);
+        }
+
+        private static List<ITurnModel> GenerateTurnsFromBoard(int[,] board)
+        {
+            List<ITurnModel> turns = new List<ITurnModel>();
+            for (int r = 0; r < board.GetLength(0); r++)
+            {
+                for (int c = 0; c < board.GetLength(1); c++)
+                {
+                    if (board[r, c] != 0)
+                    {
+                        turns.Add(
+                            new TurnModel
+                            {
+                                ColNum = c + 1,
+                                RowNum = r + 1,
+                                Num = board[r, c]
+                            }
+                        );
+                    }
+                }
+            }
+            return turns;
         }
     }
 }
